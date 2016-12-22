47626d0b435268aaf504a494c34a599e507c0fb1
GUILHERME Adamy LEAL <galeal@sispro.com.br>
2016-12-22 15:48:44 -0200

    removing dev database

--- srv_engine/settings.py
+++ srv_engine/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'srv_engine.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'dev.sqlite3'),
     }
 }
 
